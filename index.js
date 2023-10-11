d78e0949c193d236cf9d59bd75f1565bb58b5e49
MamaneHassane <hassane.mamane017@gmail.com>
2023-10-11 23:03:27 +0100

    Just starting

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const pgp = require('pg-promise')(); // Utilisation de pg-promise pour la connexion à PostgreSQL
 const app = express();
 const port = 3000;
-
+const cors = require('cors');
 // Configuration de la connexion à la base de données PostgreSQL
 const dbConfig = {
   host: 'localhost',
@@ -19,24 +19,18 @@ app.use(express.json());
 //Gerer CORS
 app.use(cors());
 // Route pour insérer des données spatiales
-app.post('/inserer-donnees-spatiales', async (req, res) => {
+app.post('/postcitizen', async (req, res) => {
   try {
-    const { nom, geometrie } = req.body; // Supposons que vous envoyez le nom et la géométrie dans le corps de la requête JSON
-
-    // Requête SQL pour insérer des données spatiales dans la table PostGIS
-    const insertQuery = `
-      INSERT INTO nom_de_votre_table (nom, geometrie)
-      VALUES ($1, ST_GeomFromGeoJSON($2))
-    `;
-
-    await db.none(insertQuery, [nom, geometrie]);
-
-    res.status(200).json({ message: 'Données insérées avec succès' });
+    
+    res.status(200).json({ 
+      message:'Données insérées avec succès'
+    });
   } catch (error) {
-    console.error('Erreur lors de l\'insertion des données spatiales :', error);
-    res.status(500).json({ error: 'Erreur lors de l\'insertion des données spatiales' });
+    console.error('Erreur lors de l\'insertion de la personne', error);
+    res.status(500).json({ error: 'Erreur lors de l\'insertion de la personne' });
   }
 });
+
 
 // Démarrer le serveur Express.js
 app.listen(port, () => {
