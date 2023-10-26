8a7b52f1714c36100180339dec17a6a70da201b9
MamaneHassane <hassane.mamane017@gmail.com>
2023-10-26 16:42:29 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 
 const dbConfig = {
   host: 'localhost',
-  port: 5432, // Le port doit être défini correctement, au cas où il est dans docker
+  port: 5432,
   database: 'spatial2',
   user: 'postgres',
   password: '123',
@@ -19,19 +19,13 @@ app.use(cors());
 
 app.post('/postcitizen', async (req, res) => {
   try {
-    const { LocationAtATime } = req.body;
+    const { user_id, latitude, longitude } = req.body;
 
     const insertQuery = `
-      INSERT INTO Location_At_Time (user_id, latitude, longitude, date, time)
-      VALUES ($1, $2, $3, $4, $5)
+      INSERT INTO nom_de_la_table (user_id, latitude, longitude, date, time)
+      VALUES ($1, $2, $3, current_date, current_time)
     `;
-    await db.none(insertQuery, [
-      LocationAtATime.user_id,
-      LocationAtATime.latitude,
-      LocationAtATime.longitude,
-      LocationAtATime.date,
-      LocationAtATime.time
-    ]);
+    await db.none(insertQuery, [user_id, latitude, longitude]);
 
     res.status(200).json({ message: 'Données insérées avec succès' });
   } catch (error) {
@@ -39,34 +33,18 @@ app.post('/postcitizen', async (req, res) => {
     res.status(500).json({ error: 'Erreur lors de l\'insertion de la personne' });
   }
 });
-app.put(`/updatecitizen/${datas}`, async (req, res) => {
-  try {
-    res.status(200).json({ 
-      message:'Données mises à jour avec succès'
-    });
-  } catch (error) {
-    console.error('Erreur lors de la mise à jour de la personne', error);
-    res.status(500).json({ error: 'Erreur lors de l\'insertion de la personne' });
-  }
-});
 
 app.put('/updatecitizen/:user_id', async (req, res) => {
   try {
     const { user_id } = req.params;
-    const { LocationAtATime } = req.body;
+    const { latitude, longitude } = req.body;
 
     const updateQuery = `
-      UPDATE Location_At_Time
-      SET latitude = $2, longitude = $3, date = $4, time = $5
-      WHERE user_id = $1
+      UPDATE nom_de_la_table
+      SET latitude = $1, longitude = $2, date = current_date, time = current_time
+      WHERE user_id = $3
     `;
-    await db.none(updateQuery, [
-      user_id,
-      LocationAtATime.latitude,
-      LocationAtATime.longitude,
-      LocationAtATime.date,
-      LocationAtATime.time
-    ]);
+    await db.none(updateQuery, [latitude, longitude, user_id]);
 
     res.status(200).json({ message: 'Données mises à jour avec succès' });
   } catch (error) {
