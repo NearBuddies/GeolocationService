762ee03c966f5c36e16e260a7c83f40ec16985da
MamaneHassane <hassane.mamane017@gmail.com>
2024-01-18 00:38:41 +0100

    starting geoservice

--- SocketCrud.js
+++ SocketCrud.js
@@ -1,32 +1,36 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
+const axios = require('axios'); // Ajout de l'importation Axios
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
 const port = 3002;
 
-io.on('connection',(socket)=>{
+io.on('connection', (socket) => {
     console.log('We have a client');
-    // Posting a location 
     socket.on('postentitylocation', (data) => {
-        axios.post(`postentitylocation`, data).then(
-            (response)=>{
-                io.emit('postentitylocation',response);
-            }
-        ).catch((error)=>{console.log(error);})          
+        axios.post('http://localhost:3000/postentitylocation', data) 
+            .then((response) => {
+                socket.emit('postentitylocation', response.data); 
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     });
-    // Getting a location
+
     socket.on('getlatestentitylocation', (data) => {
-        axios.post(`getlatestentitylocation/${data.entity_id}`).then(
-            (response)=>{
-                socket.emit('getlatestentitylocation', response);
-            }
-        ).catch((error)=>{console.log(error);})          
+        axios.post(`http://localhost:3000/getlatestentitylocation/${data.entity_id}`)
+            .then((response) => {
+                socket.emit('getlatestentitylocation', response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     });
-    // I will not update or delete the location using a socket
-})
+
+});
 
 server.listen(port, () => {
-  console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
+    console.log(`Serveur Express en cours d'exécution sur le port ${port}`);
 });
