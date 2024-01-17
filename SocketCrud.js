3aaf71457af3be19a35d9574929681cfe13909e9
MamaneHassane <hassane.mamane017@gmail.com>
2024-01-18 00:39:10 +0100

    starting geoservice

--- SocketCrud.js
+++ SocketCrud.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-const axios = require('axios'); // Ajout de l'importation Axios
+const axios = require('axios'); 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
